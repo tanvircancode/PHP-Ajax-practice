6d1dba2b856a05db30fe02bcb460d14205b1fd06
Tanvir Ahmed Chowdhury <tanahmchw241996@gmail.com>
2021-07-28 23:42:32 +0600

    updated

--- data.php
+++ data.php
@@ -1,6 +1,6 @@
 <?php
 // echo "morning";
-$data = $_REQUEST;
+$data = $_POST;
 $data['result'] = 'perfect';
 $data['method'] = $_SERVER['REQUEST_METHOD'];
 echo json_encode( $data, JSON_PRETTY_PRINT );
